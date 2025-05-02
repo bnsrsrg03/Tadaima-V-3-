6f9e0c79cdd2df61caa7954fbe313a7c89f58c60
bnsrsrg03 <binsarsiregar8a@gmail.com>
2025-05-02 15:57:55 +0700

    Hapus button delete pada edit halaman

--- tadaima-v3/app/Filament/Resources/KategoriResource/Pages/EditKategori.php
+++ tadaima-v3/app/Filament/Resources/KategoriResource/Pages/EditKategori.php
@@ -13,7 +13,7 @@ class EditKategori extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            // Actions\DeleteAction::make(),
         ];
     }
 }
--- tadaima-v3/app/Filament/Resources/MenuResource/Pages/EditMenu.php
+++ tadaima-v3/app/Filament/Resources/MenuResource/Pages/EditMenu.php
@@ -13,7 +13,7 @@ class EditMenu extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            // Actions\DeleteAction::make(),
         ];
     }
 }
