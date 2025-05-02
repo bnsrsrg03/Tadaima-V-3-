adf17bfd2d9fee884c1352603c2d316a09a3cf5e
bnsrsrg03 <binsarsiregar8a@gmail.com>
2025-05-02 15:51:05 +0700

    Button Delete pada JamOperasiona dan Galeri

--- tadaima-v3/app/Filament/Resources/GaleriResource.php
+++ tadaima-v3/app/Filament/Resources/GaleriResource.php
@@ -57,6 +57,8 @@ class GaleriResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+                
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
