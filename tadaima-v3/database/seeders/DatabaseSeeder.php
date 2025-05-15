f617029e2e97f8a8c05066c86a55cead8c373098
bnsrsrg03 <binsarsiregar8a@gmail.com>
2025-05-15 13:38:20 +0700

    Akun Default Admin

--- tadaima-v3/database/seeders/DatabaseSeeder.php
+++ tadaima-v3/database/seeders/DatabaseSeeder.php
@@ -2,18 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\AdminUserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-       $this->call(KaryawanSeeder::class);
-        $this->call(MenuSeeder::class);
+        $this->call([
+            AdminUserSeeder::class,
+        ]);
     }
 }
